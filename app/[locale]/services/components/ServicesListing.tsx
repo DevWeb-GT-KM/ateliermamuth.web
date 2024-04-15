14670debe021a01cf42044dd8a36363e366621dd
Gabt88 <gabriel.tremblay.develop@gmail.com>
2024-04-14 21:58:46 -0400

    Manque vector + sections lien projet

--- app/[locale]/services/components/ServicesListing.tsx
+++ app/[locale]/services/components/ServicesListing.tsx
@@ -1,3 +1,5 @@
+import "./ServicesListing.scss";
+
 import Image from "next/image";
 
 import { Link } from "@/../navigation";
@@ -12,37 +14,44 @@ export const ServicesListing: React.FC<ServicesListingProps> = ({
   return (
     <div className="services-listing-container">
       {services?.map((service) => (
-        <div key={`${service.name}-service`} className="services-listing-item">
+        <div key={`${service?.name}-service`} className="services-listing-item">
+          <h2 className="services-listing-item-title">{service?.name}</h2>
           <div className="services-listing-item-info-container">
-            <h2
-              className="services-listing-item-title"
-              style={{ color: "blue" }}
-            >
-              {service.name}
-            </h2>
-            <p className="services-listing-item-description">
-              {service.description}
-            </p>
-            <Link
-              className="services-listing-item-link"
-              key={service._id}
-              href={{
-                pathname: "/services/[slug]",
-                params: { slug: service.slug.current },
-              }}
-              style={{ color: "red" }}
-            >
-              en savoir plus
-            </Link>
-          </div>
+            <div className="services-listing-item-description-container">
+              <p className="services-listing-item-description">
+                {service?.description}
+              </p>
+              <Link
+                className="services-listing-item-link"
+                key={service?._id}
+                href={{
+                  pathname: "/services/[slug]",
+                  params: { slug: service?.slug?.current },
+                }}
+              >
+                en savoir plus
+              </Link>
 
-          <div className="services-listing-item-image-container">
-            <Image
-              width={100}
-              height={100}
-              alt=""
-              src={service.image.asset.url}
-            />
+              <div>
+                {service?.projectTypes?.map((type: string, index: number) => (
+                  <p
+                    key={`project-type-${index}`}
+                    className="services-listing-item-project-type"
+                  >
+                    {type}
+                  </p>
+                ))}
+              </div>
+            </div>
+
+            <div className="services-listing-item-image-container">
+              <Image
+                className="services-listing-item-image"
+                src={service?.image?.asset?.url}
+                alt=""
+                fill
+              />
+            </div>
           </div>
         </div>
       ))}
--- app/[locale]/services/components/ServicesPageContainer.tsx
+++ app/[locale]/services/components/ServicesPageContainer.tsx
@@ -1,3 +1,5 @@
+import "./ServicesPageContainer.scss";
+
 import { ServicesListing } from "./ServicesListing";
 
 export type ServicesPageContainerProps = {
@@ -16,16 +18,17 @@ export const ServicesPageContainer: React.FC<ServicesPageContainerProps> = ({
       <div className="services-page-header">
         <h1 className="services-page-header-title">{data?.[0]?.pageTitle}</h1>
       </div>
-      {data?.[0].description?.map((descriptionParagraph) => (
-        <p
-          key={descriptionParagraph._key}
-          className="services-page-description"
-        >
-          {descriptionParagraph.children?.[0].text}
-        </p>
-      ))}
-      <ServicesListing services={data?.[0]?.services} />
-      <div className="services-page-projects-container"></div>
+      <div className="services-page-content">
+        <div className="services-page-content-description-container">
+          {data?.[0].description?.map((descriptionParagraph) => (
+            <p key={descriptionParagraph._key}>
+              {descriptionParagraph.children?.[0].text}
+            </p>
+          ))}
+        </div>
+        <ServicesListing services={data?.[0]?.services} />
+        <div className="services-page-projects-container"></div>
+      </div>
     </div>
   );
 };
