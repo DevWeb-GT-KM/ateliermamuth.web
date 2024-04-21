f21bf7d2e15244f56b42cb663c7fa7d07c2c857b
kristopher <kristopher.maltais.1@ulaval.ca>
2024-04-21 18:26:37 -0400

    fix error build

--- app/[locale]/services/[slug]/components/ContactUs.tsx
+++ app/[locale]/services/[slug]/components/ContactUs.tsx
@@ -12,8 +12,9 @@ export const ContactUs: React.FC<ContactUsProps> = ({ serviceType }) => {
       className={`service-page-contact-us-container service-page-contact-us-${serviceType}`}
     >
       <h1 className="service-page-contact-us-message">
-        Vous aimeriez avoir plus d'informations, écrivez-nous via notre
-        formulaire
+        {
+          "Vous aimeriez avoir plus d'informations, écrivez-nous via notre formulaire"
+        }
       </h1>
       <Link className="service-page-contact-us-link" href={{ pathname: "/" }}>
         écrivez-nous
