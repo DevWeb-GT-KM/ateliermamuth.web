45cc665f8a9760e487431806266fe0a685f49f06
kristopher <kristopher.maltais.1@ulaval.ca>
2024-04-21 18:10:33 -0400

    resolve build issues

--- app/[locale]/components/publications/Publication.tsx
+++ app/[locale]/components/publications/Publication.tsx
@@ -11,11 +11,8 @@ export const Publication: React.FC<PublicationProps> = ({ data }) => {
     <div className="home-page-publication-container">
       {data.map((publication: any, index: number) => {
         return (
-          <div className="home-page-publication-item">
-            <p
-              key={index}
-              className="home-page-publication-text"
-            >{`${publication.mediaName}, ${publication.publicationDate}`}</p>
+          <div key={index} className="home-page-publication-item">
+            <p className="home-page-publication-text">{`${publication.mediaName}, ${publication.publicationDate}`}</p>
             <Link className="home-page-publication-link" href={"/"}>
               lien
             </Link>
