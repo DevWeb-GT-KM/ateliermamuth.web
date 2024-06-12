52cdc497515fec173fc6f63db9331f3cc5f8be74
Kristopher Maltais <kristopher.maltais.1@ulaval.ca>
2024-06-12 18:41:08 -0400

    client adjustment

--- app/common/components/Project.tsx
+++ app/common/components/Project.tsx
@@ -37,8 +37,15 @@ export const Project: React.FC<ProjectProps> = ({
       />
       <div className="project-description">
         <h1 className="project-description-title">{data.name}</h1>
-        <h1 className="project-description-description" dangerouslySetInnerHTML={{ __html: buildSeeMoreString(data.shortDescription, MAX_CHARACTER_DISPLAYED) }}>
-        </h1>
+        <h1
+          className="project-description-description"
+          dangerouslySetInnerHTML={{
+            __html: buildSeeMoreString(
+              data.shortDescription,
+              MAX_CHARACTER_DISPLAYED
+            ),
+          }}
+        ></h1>
         {!isSecondaryProject && (
           <div className="project-description-project-types">
             {data.projectTypes.map((projectType: any, index: number) => {
