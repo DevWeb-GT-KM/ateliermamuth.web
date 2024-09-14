67355dab42912a00505da4db255198826e51d7c6
Gabt88 <gabriel.tremblay34@gmail.com>
2024-09-14 13:56:45 -0400

    Add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -27,6 +27,61 @@ const nextConfig = {
         destination: "/fr/services",
         permanent: true,
       },
+      {
+        source: "/portfolio",
+        destination: "/fr/projets",
+        permanent: true,
+      },
+      {
+        source: "/contact",
+        destination: "/fr/contact",
+        permanent: true,
+      },
+      {
+        source: "/a-propos",
+        destination: "/fr/a-propos",
+        permanent: true,
+      },
+      {
+        source: "/mmfm",
+        destination: "/fr/projets/mmfm",
+        permanent: true,
+      },
+      {
+        source: "/winner-dinner",
+        destination: "/fr/projets/winner-dinner",
+        permanent: true,
+      },
+      {
+        source: "/couleur-plateau",
+        destination: "/fr/projets/couleur-plateau",
+        permanent: true,
+      },
+      {
+        source: "/luma-cabin",
+        destination: "/fr/projets/luma-cabin",
+        permanent: true,
+      },
+      {
+        source: "/zz-countertop",
+        destination: "/fr/projets/zz-countertop",
+        permanent: true,
+      },
+      {
+        source: "/eau-claire-sur-terreau",
+        destination: "/fr/projets/eau-claire-sur-terreau",
+        permanent: true,
+      },
+      {
+        source: "/codem-cje",
+        destination: "/fr/projets/codem-cje",
+        permanent: true,
+      },
+      {
+        source: "/eau-claire-sur-terreau",
+        destination: "/fr/projets/eau-claire-sur-terreau",
+        permanent: true,
+      },
     ];
   },
 };
